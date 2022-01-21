a7d967693217d0f46bb0f53f12f85d0f62e51a39
Clément Salaün <salaun.clement@gmail.com>
2022-01-21 17:51:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,7 +56,8 @@ const config = {
           },
           sidebarPath: require.resolve('./sidebarsCloud.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/numary/docs/edit/main/docs/',
+          //editUrl: 'https://github.com/numary/docs/edit/main/docs/',
+          routeBasePath: '/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
