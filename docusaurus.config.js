852061b507e1d1606ba94891ff40c831b8873a90
Clément Salaün <salaun.clement@gmail.com>
2022-05-06 11:07:37 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'The open source foundation you need to build and scale money-movements within your app',
   url: 'https://docs.numary.com/',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'numary', // Usually your GitHub org/user name.
