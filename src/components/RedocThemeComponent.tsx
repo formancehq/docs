f65d60de7dc786017a36b57c7cd94b1247f9f6d9
Adhil Rumais <adhilrumais007@gmail.com>
2024-07-10 02:07:51 +0530

    feat: customized light and dark themes for redocly

---
+++ src/components/RedocThemeComponent.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { RedocStandalone } from "redoc";
+import { useColorMode } from "@docusaurus/theme-common";
+import v1json from "../../openapi/v1.10.json";
+import v2json from "../../openapi/v2.0.json";
+import genericConnector from "../../openapi/generic-connector.json";
+
+const redoclySpecPaths = {
+  "api-v1-10": v1json,
+  "api-v2-0": v2json,
+  "api-generic-connector": genericConnector,
+};
+
+const darkTheme = {
+  breakpoints: {
+    medium: "170rem",
+    large: "256rem",
+  },
+  colors: {
+    primary: {
+      main: "#7a9394",
+    },
+    text: {
+      primary: "#d5e1e1",
+      secondary: "#d5e1e1",
+    },
+    http: {
+      link: "#7a9394",
+    },
+    border: {
+      dark: "#d5e1e1",
+    },
+  },
+  sidebar: {
+    backgroundColor: "#011f23",
+    groupItems: { activeBackgroundColor: "#0e2a2d" },
+    level1Items: { activeBackgroundColor: "#133336" },
+    textColor: "#d5e1e1",
+    rightLineColor: "#0e2a2d",
+    fontFamily: "Polymath",
+  },
+  typography: {
+    code: { color: "#426367" },
+    headings: { fontFamily: "Polymath" },
+    links: { color: "#7a9394" },
+    fontFamily: "Polymath",
+    fontSize: "1rem",
+  },
+  rightPanel: {
+    backgroundColor: "#0e2a2d",
+  },
+  codeBlock: {
+    backgroundColor: "#000000",
+  },
+};
+
+const lightTheme = {
+  breakpoints: {
+    medium: "170rem",
+    large: "256rem",
+  },
+  colors: {
+    primary: {
+      main: "#7a9394",
+    },
+    text: {
+      primary: "#011f23",
+    },
+  },
+  sidebar: {
+    backgroundColor: "#f6f9f9",
+    groupItems: { activeBackgroundColor: "#cdcdcd51" },
+    level1Items: { activeBackgroundColor: "#e7e5e551" },
+    textColor: "#011f23",
+    fontFamily: "Polymath",
+  },
+  typography: {
+    code: { color: "#ff0000" },
+    headings: { fontFamily: "Polymath" },
+    fontFamily: "Polymath",
+    fontSize: "1rem",
+  },
+  rightPanel: {
+    backgroundColor: "#303846",
+  },
+  codeBlock: {
+    backgroundColor: "#000000",
+  },
+};
+
+const RedocThemeComponent = ({ specID }) => {
+  const { colorMode } = useColorMode();
+  const [theme, setTheme] = useState(lightTheme);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTheme(colorMode === "dark" ? darkTheme : lightTheme);
+    const timer = setTimeout(() => setLoading(false));
+    return () => clearTimeout(timer);
+  }, [colorMode]);
+  return (
+    <>
+      {loading ? (
+        <div className="loading-overlay"></div>
+      ) : (
+        <RedocStandalone
+          options={{
+            disableSearch: true,
+            hideRequestPayloadSample: true,
+            hideLoading: false,
+            nativeScrollbars: false,
+            hideSecuritySection: false,
+            theme,
+          }}
+          spec={redoclySpecPaths[specID]}
+        />
+      )}
+    </>
+  );
+};
+
+export default RedocThemeComponent;
